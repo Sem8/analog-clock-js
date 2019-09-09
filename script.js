9ddf8a9d7ca2802dfa02f0cace9d71700d936c3c
Sem8 <mssemmi8@gmail.com>
2019-09-08 23:19:06 -0400

    finished writing javascript for the analog clock functionality

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 setInterval(setClock, 1000);
 
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
 function setClock() {
     const currentDate = new Date();
     const secondsRatio = currentDate.getSeconds() / 60;
@@ -8,8 +12,17 @@ function setClock() {
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
     // console.log(currentDate.getMinutes())
     // console.log('currentDate.getMinutes() / 60', currentDate.getMinutes() / 60);
-    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
 
 }
-// setClock();
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
 
